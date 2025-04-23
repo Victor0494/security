72366fc858002b541e04b1d77ee7ee70823da46c
Victor Martins <victormartins0494@gmail.com>
2025-04-23 17:00:01 -0300

    Implementation of RememberMe

--- src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
+++ src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -2,6 +2,7 @@ package med.voll.web_application.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -38,6 +39,12 @@ public class ConfiguracoesSeguranca {
                         .defaultSuccessUrl("/").permitAll())
                     .logout(logout -> logout
                             .logoutSuccessUrl("/login?logout").permitAll())
+                    .rememberMe(rememberMe -> rememberMe //Token para que o usuário não precise ficar sempre logando
+                            .key("lembrarDeMim")
+                            .alwaysRemember(true)
+//                                    .tokenValiditySeconds()
+                    )
+                    .csrf(Customizer.withDefaults())
                     .build();
         }
 }
